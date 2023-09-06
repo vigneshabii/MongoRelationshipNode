c88920f691a56fffedbc810422891adef71b5243
vigneshabii <vigneshcrk@gmail.com>
2023-09-06 11:46:29 +0530

    add aggregate function retrive customer_phones documents

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const dbo=require('./db');
 (async () => {
     let database=await dbo.getDatabase();
     let collection=database.collection('customers');
-    let find=await collection.find({}).toArray();
+    let find=await collection.aggregate([
+        {$lookup:{
+            from: "customer_phones",
+            localField:"_id",
+            foreignField:"customer_id",
+            as: "Cutomer_details"
+        }}
+    ]).toArray();
 
-    console.log(find);
+    console.log(JSON.stringify(find));
 })();
